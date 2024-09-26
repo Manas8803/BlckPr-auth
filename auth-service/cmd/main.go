045d0353969f9a2f3009e79a355c2f2fd2ee5ad8
Manas8803 <114303599+Manas8803@users.noreply.github.com>
2024-09-27 01:53:52 +0530

    chore: Remove unecessary env(WALLET_URL)

--- auth-service/cmd/main.go
+++ auth-service/cmd/main.go
@@ -1,18 +1,11 @@
 package main
 
 import (
-	"auth-service/docs"
-	"auth-service/lib/configs"
-	"auth-service/main-app/controllers"
-	"auth-service/main-app/routes"
 	"context"
-	"log"
-	"os"
 
 	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
 	ginadapter "github.com/awslabs/aws-lambda-go-api-proxy/gin"
-	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
 )
 
 //	@title			Auth API
@@ -45,28 +38,28 @@ func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.API
 }
 
 func main() {
-	// lambda.Start(Handler)
-	err := godotenv.Load("./.env")
-	if err != nil {
-		log.Println("Error loading environment")
-		return
-	}
-	prod := os.Getenv("RELEASE_MODE")
-	if prod == "true" {
-		gin.SetMode(gin.ReleaseMode)
-	}
-	router := gin.Default()
-	docs.SwaggerInfo.BasePath = "/api/v1"
+	lambda.Start(Handler)
+	// err := godotenv.Load("./.env")
+	// if err != nil {
+	// 	log.Println("Error loading environment")
+	// 	return
+	// }
+	// prod := os.Getenv("RELEASE_MODE")
+	// if prod == "true" {
+	// 	gin.SetMode(gin.ReleaseMode)
+	// }
+	// router := gin.Default()
+	// docs.SwaggerInfo.BasePath = "/api/v1"
 
-	api := router.Group("/api/v1")
-	//* Passing the router to all user(auth-service) routes.
-	routes.UserRoute(api)
+	// api := router.Group("/api/v1")
+	// //* Passing the router to all user(auth-service) routes.
+	// routes.UserRoute(api)
 
-	//* Connecting to DB
-	configs.ConnectDB()
-	router.GET("/", controllers.BaseRoute)
-	// router.GET("/api/v1/docs/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+	// //* Connecting to DB
+	// configs.ConnectDB()
+	// router.GET("/", controllers.BaseRoute)
+	// // router.GET("/api/v1/docs/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 
-	router.Run("localhost:8080")
-	configs.CloseDB()
+	// router.Run("localhost:8080")
+	// configs.CloseDB()
 }
