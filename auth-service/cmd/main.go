8b8a626a5ced8d6cc7f22f98079e8f6a3d8da798
Manas8803 <114303599+Manas8803@users.noreply.github.com>
2024-09-27 01:54:38 +0530

    chore: Remove unecessary env(WALLET_URL)

--- auth-service/cmd/main.go
+++ auth-service/cmd/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"auth-service/docs"
+	"auth-service/lib/configs"
+	"auth-service/main-app/controllers"
+	"auth-service/main-app/routes"
 	"context"
+	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	ginadapter "github.com/awslabs/aws-lambda-go-api-proxy/gin"
+	"github.com/gin-gonic/gin"
 )
 
 //	@title			Auth API
@@ -15,23 +21,23 @@ import (
 // @BasePath	/api/v1
 var ginLambda *ginadapter.GinLambda
 
-// func init() {
-// 	prod := os.Getenv("RELEASE_MODE")
-// 	if prod == "true" {
-// 		gin.SetMode(gin.ReleaseMode)
-// 	}
-// 	router := gin.Default()
-// 	docs.SwaggerInfo.BasePath = "/api/v1"
+func init() {
+	prod := os.Getenv("RELEASE_MODE")
+	if prod == "true" {
+		gin.SetMode(gin.ReleaseMode)
+	}
+	router := gin.Default()
+	docs.SwaggerInfo.BasePath = "/api/v1"
 
-// 	api := router.Group("/api/v1")
-// 	//* Passing the router to all user(auth-service) routes.
-// 	routes.UserRoute(api)
+	api := router.Group("/api/v1")
+	//* Passing the router to all user(auth-service) routes.
+	routes.UserRoute(api)
 
-// 	//* Connecting to DB
-// 	configs.ConnectDB()
-// 	router.GET("/", controllers.BaseRoute)
-// 	ginLambda = ginadapter.New(router)
-// }
+	//* Connecting to DB
+	configs.ConnectDB()
+	router.GET("/", controllers.BaseRoute)
+	ginLambda = ginadapter.New(router)
+}
 
 func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	return ginLambda.ProxyWithContext(ctx, req)
